f9ff48dc4ca2e081a25665b0ccc8e9bfe9ea8569
AAE <alysae2017@gmail.com>
2024-03-20 00:32:09 -0400

    added files to utils folder and updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+const auth = setContext((_, {headers}) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+})
+const httpLink = createHttpLink({uri: '/graphql'});
+const client = new ApolloClient({
+  link: auth.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
-    <>
+   
+      <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+      </ApolloProvider >
+   
   );
 }
 
