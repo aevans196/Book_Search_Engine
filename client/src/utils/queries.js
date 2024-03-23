67cca08d7339625ea7cda77bb92f785de77061dd
AAE <alysae2017@gmail.com>
2024-03-23 10:29:33 -0400

    added comments

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,9 @@
+// Import the gql function from the Apollo Client library
 import { gql } from '@apollo/client';
 
+// Define a GraphQL query to retrieve user information, including saved books
+// This query requires the user's ID as an input parameter
+// It returns the user's ID, username, email, book count, and an array of saved books
 export const QUERY_GET_ME = gql`
 query me ($_id: ID!) {
     me(userId: $_id) {
