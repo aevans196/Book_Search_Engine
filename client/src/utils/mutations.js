79a6d604a5ca23eb3e61f564782ec7b013f4f0a0
AAE <alysae2017@gmail.com>
2024-03-23 10:27:20 -0400

    added missing lines

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,52 +1,84 @@
-import { gql } from '@apollo/client';
+import {gql} from '@apollo/client'
+// Define a GraphQL mutation for logging in a user
+// This mutation takes email and password as input parameters
+// It returns a token and user information if the login is successful
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
 
+// Define a GraphQL mutation for adding a new user
+// This mutation takes username, email, and password as input parameters
+// It returns a token and user information if the user is successfully added
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-        token
-        user {
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    authors
+                    bookId
+                    image
+                    link
+                    title
+                    description
+                }
+            }
+        }
+    }
+`;
+
+// Define a GraphQL mutation for saving a book to a user's list of saved books
+// This mutation takes a BookInput object as input parameter
+// It returns updated user information with the newly saved book
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: BookInput!) {
+        saveBook(input: $input) {
             _id
             username
             email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
         }
     }
-}`
+`;
 
-export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-        token
-        user {
+// Define a GraphQL mutation for deleting a book from a user's list of saved books
+// This mutation takes userId and bookId as input parameters
+// It returns updated user information after deleting the specified book
+export const DELETE_BOOK = gql`
+    mutation deleteBook($userId: ID!, $bookId: String!) {
+        deleteBook(userId: $userId, bookId: $bookId) {
             _id
+            username
             email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
         }
     }
-}`
-
-export const REMOVE_BOOK = gql`
-mutation removeBook($userId: ID!, $bookId: String!) {
-    removeBook(userId: $userId, bookId: $bookId) {
-        savedBooks {
-            bookId
-            authors
-            description
-            image
-            title
-            link
-        }
-    }
-}`
-
-export const SAVE_BOOK = gql`
-mutation saveBook($input: BookInput!) {
-    saveBook(input: $input) {
-        savedBooks {
-            bookId
-            authors
-            description
-            image
-            title
-            link
-        }
-    }
-}`
+`;
