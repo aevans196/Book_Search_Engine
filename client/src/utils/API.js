70f073b684c5006fc777fa2ae7a00d2121f92edd
AAE <alysae2017@gmail.com>
2024-03-23 10:33:54 -0400

    updated file and added comments

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,57 +1,8 @@
-// route to get logged in user's info (needs the token)
-export const getMe = (token) => {
-  return fetch('/api/users/me', {
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-  });
-};
-
-export const createUser = (userData) => {
-  return fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-};
-
-export const loginUser = (userData) => {
-  return fetch('/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-};
-
-// save book data for a logged in user
-export const saveBook = (bookData, token) => {
-  return fetch('/api/users', {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(bookData),
-  });
-};
-
-// remove saved book data for a logged in user
-export const deleteBook = (bookId, token) => {
-  return fetch(`/api/users/books/${bookId}`, {
-    method: 'DELETE',
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
-};
-
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
+// Function for sending a search request to the Google Books API
+// The query parameter represents the user-entered search query
+// Example usage: searchGoogleBooks('twilight')
 export const searchGoogleBooks = (query) => {
+  // Sending a GET request to the Google Books API with the provided query
+  // The query parameter is appended to the API URL
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
